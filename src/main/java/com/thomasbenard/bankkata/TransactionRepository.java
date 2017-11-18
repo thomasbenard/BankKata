3ad6798c1c55917206166833affbc80bc4372714
Thomas Bénard <m.thomas.benard@gmail.com>
2017-11-19 00:19:23 +0100

    Create the TransactionRepository class

---
+++ src/main/java/com/thomasbenard/bankkata/TransactionRepository.java
@@ -0,0 +1,9 @@
+package com.thomasbenard.bankkata;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class TransactionRepository {
+    void addDeposit(int amount) {
+        throw new NotImplementedException();
+    }
+}
