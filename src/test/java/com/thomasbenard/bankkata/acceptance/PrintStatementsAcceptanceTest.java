a405361391192a6e05c89f7627de66e4aac7ef04
Thomas Bénard <m.thomas.benard@gmail.com>
2017-11-18 22:24:45 +0100

    Add the acceptance test
    Given a client makes a deposit of 1000 on 10-01-2012
       and a deposit of 2000 on 13-01-2012
       and a withdrawal of 500 on 14-01-2012
    When she prints her bank statement
    Then she would see:
       DATE | AMOUNT | BALANCE
       14/01/2012 | -500 | 2500
       13/01/2012 | 2000 | 3000
       10/01/2012 | 1000 | 1000

---
+++ src/test/java/com/thomasbenard/bankkata/acceptance/PrintStatementsAcceptanceTest.java
@@ -0,0 +1,34 @@
+package com.thomasbenard.bankkata.acceptance;
+
+import com.thomasbenard.bankkata.Account;
+import com.thomasbenard.bankkata.Console;
+import org.junit.Test;
+import org.mockito.InOrder;
+
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.mock;
+
+public class PrintStatementsAcceptanceTest {
+
+    private Console console = mock(Console.class);
+    private Account account;
+
+    @Test
+    public void print_all_transactions_in_reverse_chronological_order_and_with_good_balance() throws Exception {
+        // Given ...
+        account.deposit(1000);
+        account.deposit(2000);
+        account.withdraw(500);
+
+        // When ...
+        account.printStatements();
+
+        // Then ...
+        InOrder inOrder = inOrder(console);
+        inOrder.verify(console).print("DATE | AMOUNT | BALANCE");
+        inOrder.verify(console).print("14/11/2017 | -500 | 2500");
+        inOrder.verify(console).print("13/11/2017 | 2000 | 3000");
+        inOrder.verify(console).print("10/11/2017 | 1000 | 1000");
+        inOrder.verifyNoMoreInteractions();
+    }
+}
