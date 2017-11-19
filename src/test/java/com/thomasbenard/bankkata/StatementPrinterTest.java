ab7edb0e51302b564aafbaad1920cfa344168e2f
Thomas Bénard <m.thomas.benard@gmail.com>
2017-11-19 23:49:16 +0100

    StatementPrinter should always print the header

---
+++ src/test/java/com/thomasbenard/bankkata/StatementPrinterTest.java
@@ -0,0 +1,24 @@
+package com.thomasbenard.bankkata;
+
+import org.junit.Test;
+import org.mockito.InOrder;
+
+import java.util.List;
+
+import static java.util.Collections.emptyList;
+import static org.mockito.Mockito.*;
+
+public class StatementPrinterTest {
+
+    private static final List<Transaction> NO_TRANSACTIONS = emptyList();
+    private Console console = mock(Console.class);
+    private StatementPrinter statementPrinter = new StatementPrinter();
+    private final InOrder inOrder = inOrder(console);
+
+    @Test
+    public void always_prints_the_header() throws Exception {
+        statementPrinter.print(NO_TRANSACTIONS);
+        inOrder.verify(console).print("DATE | AMOUNT | BALANCE");
+        inOrder.verifyNoMoreInteractions();
+    }
+}
