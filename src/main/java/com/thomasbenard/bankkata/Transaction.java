18b994cba385bc507c56aef64835e75b4ca12634
Thomas Bénard <m.thomas.benard@gmail.com>
2017-11-19 22:09:46 +0100

    Make the test pass

--- src/main/java/com/thomasbenard/bankkata/Transaction.java
+++ src/main/java/com/thomasbenard/bankkata/Transaction.java
@@ -8,4 +8,21 @@ class Transaction {
         this.date = date;
         this.amount = amount;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Transaction that = (Transaction) o;
+
+        return amount == that.amount && (date != null ? date.equals(that.date) : that.date == null);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = date != null ? date.hashCode() : 0;
+        result = 31 * result + amount;
+        return result;
+    }
 }
