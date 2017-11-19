20b653bb98498989800792c71495bb7a412e6c66
Thomas Bénard <m.thomas.benard@gmail.com>
2017-11-19 21:06:06 +0100

    Add a test to check if we can print a statement
    Once again, we need to defer the responsibility of statement printing  to another class. The goal of the Account class should only be to put the different pieces together.

--- src/test/java/com/thomasbenard/bankkata/AccountTest.java
+++ src/test/java/com/thomasbenard/bankkata/AccountTest.java
@@ -2,8 +2,11 @@ package com.thomasbenard.bankkata;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class AccountTest {
 
@@ -20,5 +23,13 @@ public class AccountTest {
     public void stores_a_withdrawal() throws Exception {
         account.withdraw(100);
         verify(transactionRepository).addWithdrawal(100);
+    }
+
+    @Test
+    public void prints_a_statement() throws Exception {
+        List<Transaction> transactions = new ArrayList<>();
+        when(transactionRepository.allTransactions()).thenReturn(transactions);
+        account.printStatements();
+        verify(statementPrinter).print(transactions);
     }
 }
