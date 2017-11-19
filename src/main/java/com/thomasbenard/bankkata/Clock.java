4229d4570a6e239fb8163cf002ff2ce914871fe8
Thomas Bénard <m.thomas.benard@gmail.com>
2017-11-19 23:27:00 +0100

    Add the date formatting
    The returned date is still hard coded, but we got the date formatting part figured out. Next, we need to find a way to use the real date and still have passing tests

--- src/main/java/com/thomasbenard/bankkata/Clock.java
+++ src/main/java/com/thomasbenard/bankkata/Clock.java
@@ -1,7 +1,19 @@
 package com.thomasbenard.bankkata;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 public class Clock {
+
+    private static final SimpleDateFormat DD_MM_YYYY = new SimpleDateFormat("dd/MM/yyyy");
+
     String todayAsString() {
-        return "17/11/2017";
+        return DD_MM_YYYY.format(today());
+    }
+
+    private Date today() {
+        return new GregorianCalendar(2017, Calendar.NOVEMBER, 17).getTime();
     }
 }
