47bd44f778fa367d0da573fee223b12890760fe7
Thomas Bénard <m.thomas.benard@gmail.com>
2017-11-20 00:10:26 +0100

    StatementPrinter should be able to print one transaction

--- src/test/java/com/thomasbenard/bankkata/StatementPrinterTest.java
+++ src/test/java/com/thomasbenard/bankkata/StatementPrinterTest.java
@@ -6,7 +6,9 @@ import org.mockito.InOrder;
 import java.util.List;
 
 import static java.util.Collections.emptyList;
-import static org.mockito.Mockito.*;
+import static java.util.Collections.singletonList;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.mock;
 
 public class StatementPrinterTest {
 
@@ -20,6 +22,15 @@ public class StatementPrinterTest {
     public void always_prints_the_header() throws Exception {
         statementPrinter.print(NO_TRANSACTIONS);
         inOrder.verify(console).print("DATE | AMOUNT | BALANCE");
+        inOrder.verifyNoMoreInteractions();
+    }
+
+    @Test
+    public void prints_one_transaction() throws Exception {
+        List<Transaction> transactions = singletonList(new Transaction("13/11/2017", 200));
+        statementPrinter.print(transactions);
+        inOrder.verify(console).print("DATE | AMOUNT | BALANCE");
+        inOrder.verify(console).print("13/11/2017 | 200 | 200");
         inOrder.verifyNoMoreInteractions();
     }
 }
