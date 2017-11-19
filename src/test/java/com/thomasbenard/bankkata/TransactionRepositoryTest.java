cdb6cb626671d40ee309ec1b60e9b3cb4690f6f8
Thomas Bénard <m.thomas.benard@gmail.com>
2017-11-19 21:56:02 +0100

    Add a mechanism to get the date
    In order to create a Transaction, we need to have the date of its creation. It will be job of the Clock class to handle that.

--- src/test/java/com/thomasbenard/bankkata/TransactionRepositoryTest.java
+++ src/test/java/com/thomasbenard/bankkata/TransactionRepositoryTest.java
@@ -6,13 +6,17 @@ import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class TransactionRepositoryTest {
 
-    private final TransactionRepository transactionRepository = new TransactionRepository();
+    private Clock clock = mock(Clock.class);
+    private final TransactionRepository transactionRepository = new TransactionRepository(clock);
 
     @Test
     public void deposit_creates_and_stores_a_deposit_transaction() throws Exception {
+        when(clock.todayAsString()).thenReturn("18/11/2017");
         transactionRepository.addDeposit(100);
         List<Transaction> transactions = transactionRepository.allTransactions();
         assertThat(transactions, contains(transaction("18/11/2017", 100)));
