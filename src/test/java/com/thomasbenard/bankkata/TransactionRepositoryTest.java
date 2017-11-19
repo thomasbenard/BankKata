2c7f5fdd88bcb51338de8b29f69eec165da45b9b
Thomas Bénard <m.thomas.benard@gmail.com>
2017-11-19 21:29:25 +0100

    Add a test for the TransactionRepository class
    It checks whether the deposit function creates and stores a deposit Transaction.

---
+++ src/test/java/com/thomasbenard/bankkata/TransactionRepositoryTest.java
@@ -0,0 +1,25 @@
+package com.thomasbenard.bankkata;
+
+import org.junit.Test;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+
+public class TransactionRepositoryTest {
+
+    private final TransactionRepository transactionRepository = new TransactionRepository();
+
+    @Test
+    public void deposit_creates_and_stores_a_deposit_transaction() throws Exception {
+        transactionRepository.addDeposit(100);
+        List<Transaction> transactions = transactionRepository.allTransactions();
+        assertThat(transactions, contains(transaction("18/11/2017", 100)));
+    }
+
+    private Transaction transaction(String date, int amount) {
+        throw new NotImplementedException();
+    }
+}
