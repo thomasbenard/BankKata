d1434d8b65523c0ef38ef21db592870601585fad
thomasbenard <m.thomas.benard@protonmail.com>
2019-03-11 14:03:50 +0100

    Remove unnecessary tests

--- src/test/java/com/thomasbenard/bankkata/AccountMust.java
+++ src/test/java/com/thomasbenard/bankkata/AccountMust.java
@@ -13,21 +13,9 @@ public class AccountMust {
     private Account account = new Account(statementRepository, clock);
 
     @Test
-    public void add_statement_to_repository_when_making_a_deposit_of_100() {
+    public void add_statement_to_repository_when_making_a_deposit() {
         account.deposit(new Money(100));
         assertThat(statementRepository.statements(), containsString("100.0 2019/03/10"));
     }
 
-    @Test
-    public void add_statement_to_repository_when_making_a_deposit_of_1000() {
-        account.deposit(new Money(1000));
-        assertThat(statementRepository.statements(), containsString("1000.0 2019/03/10"));
-    }
-
-    @Test
-    public void add_statement_to_repository_when_making_a_deposit_on_another_date() {
-        clock.setDate(new Date(2019, Month.MARCH, 11));
-        account.deposit(new Money(1000));
-        assertThat(statementRepository.statements(), containsString("1000.0 2019/03/11"));
-    }
 }
